8631fb34572559c4d8bfe214d340b8d96871effb
Soju <soujatamaji16@gmail.com>
2026-03-03 10:19:21 +0530

    adding code

---
+++ OOPSBannerUC3.java
@@ -0,0 +1,16 @@
+public class OOPSBannerUC3 {
+    public static void main(String[] args) {
+
+        System.out.println(String.join("\n",
+
+                String.join("", "  *****  ", "  *****  ", " ******  ", "  *****  "),
+                String.join("", " *     * ", " *     * ", " *     * ", " *     * "),
+                String.join("", "*       *", "*       *", " *     * ", "*        "),
+                String.join("", "*       *", "*       *", " ******  ", "  *****  "),
+                String.join("", "*       *", "*       *", " *       ", "       * "),
+                String.join("", " *     * ", " *     * ", " *       ", " *     * "),
+                String.join("", "  *****  ", "  *****  ", " *       ", "  *****  ")
+
+        ));
+    }
+}
